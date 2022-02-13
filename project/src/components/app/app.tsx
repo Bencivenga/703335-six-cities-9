78a7319b9870822f58795dea7afe05da7bdb7159
Mikhail Payusov <gustonauseagiusto@gmail.com>
2022-02-13 20:05:21 +0300

    Разбивает верстку на react-компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppScreenProps = {
+  placeOffersCount: number;
+};
+
+function App({placeOffersCount}: AppScreenProps): JSX.Element {
+  return <MainPage placeOffersCount={placeOffersCount}/>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Settings = {
+  PLACE_OFFERS_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      placeOffersCount = {Settings.PLACE_OFFERS_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
